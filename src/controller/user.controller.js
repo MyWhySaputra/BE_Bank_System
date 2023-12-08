448e8083dda2dc1cd5bb57b2a88fcd94bf74b21d
MyWahyuSap <wahyusaputra222000@gmail.com>
2023-12-08 20:31:41 +0700

    08/12/2023

---
+++ src/controller/user.controller.js
@@ -0,0 +1,305 @@
+const { ResponseTemplate } = require('../helper/template.helper')
+const { HashPassword } = require('../helper/hash_pass_helper')
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+const imagekit = require('../lib/imagekit')
+
+async function Register(req, res) {
+
+    const { name, email, password, profile_picture, identity_type, identity_number, address } = req.body
+
+    const hashPass = await HashPassword(password)
+
+    const payload = {
+        name,
+        email,
+        password: hashPass,
+        profile: {
+            create: {
+                profile_picture,
+                identity_type,
+                identity_number,
+                address
+            }
+        }
+    }
+
+    const emailUser = await prisma.user.findUnique({
+        where: {email: payload.email},
+    });
+
+    if (emailUser) {
+        let resp = ResponseTemplate(null, 'Email already exist', null, 404)
+        res.status(404).json(resp)
+        return
+    }
+
+    if (req.body.identity_type !== 'KTP' && req.body.identity_type !== 'SIM') {
+        let resp = ResponseTemplate(null, 'identity type must be KTP or SIM', null, 404)
+        res.status(404).json(resp)
+        return
+    }
+
+    try {
+
+        const stringFile = req.file.buffer.toString("base64");
+    
+        const uploadFile = await imagekit.upload({
+            fileName: req.file.originalname,
+            file: stringFile,
+        })
+        
+        await prisma.user.create({
+            data: {
+                ...payload,
+                profile: {
+                    update: {
+                        profile_picture: uploadFile.url
+                    }
+                }
+            },
+            include: {
+                profile: true
+            }
+        });
+
+        const userView = await prisma.user.findUnique({
+            where: {
+                email: payload.email
+            },
+            select: {
+                id: true,
+                name: true,
+                email: true,
+                profile: {
+                    select: {
+                        profile_picture: true,
+                        identity_type: true,
+                        identity_number: true,
+                        address: true
+                    }
+                }
+            },
+        });
+
+        let resp = ResponseTemplate(userView, 'success', null, 200)
+        res.status(200).json(resp);
+        return
+
+    } catch (error) {
+        let resp = ResponseTemplate(null, 'internal server error', error, 500)
+        res.status(500).json(resp)
+        return
+
+    }
+}
+
+async function Get(req, res) {
+
+    const { name, email, password, page = 1, limit = 10 } = req.query
+
+    const payload = {}
+
+    if (name) payload.name = name
+    if (email) payload.email = email
+    if (password) payload.password = password
+
+    try {
+        const skip = ( page - 1 ) * limit
+
+        //informasi total data keseluruhan 
+        const resultCount = await prisma.user.count() // integer jumlah total data user
+
+        //generated total page
+        const totalPage = Math.ceil( resultCount / limit)
+
+        const users = await prisma.user.findMany({
+            //take : 10,
+            take : parseInt(limit),
+            //skip : 10
+            skip:skip,
+            where: payload,
+            select: {
+                id: true,
+                name: true,
+                email: true,
+                profile: {
+                    select: {
+                        identity_type: true,
+                        identity_number: true,
+                        address: true
+                    }
+                }
+            },
+        });
+        
+        const pagination = {
+            current_page: page - 0, // ini - 0 merubah menjadi integer
+            total_page : totalPage,
+            total_data: resultCount,
+            data: users
+        }
+        const cekUser = (objectName) => {
+            return Object.keys(objectName).length === 0
+        }
+        
+        if (cekUser(users) === true) {
+            let resp = ResponseTemplate(null, 'data not found', null, 404)
+            res.json(resp)
+            return
+        }
+
+        let resp = ResponseTemplate(pagination, 'success', null, 200)
+        res.json(resp)
+        return
+
+    } catch (error) {
+        let resp = ResponseTemplate(null, 'internal server error', error, 500)
+        res.json(resp)
+        return
+    }
+}
+
+async function GetByPK(req, res) {
+
+    const { id } = req.params
+
+    try {
+        const users = await prisma.user.findUnique({
+            where: {
+                id: Number(id)
+            },
+            select: {
+                id: true,
+                name: true,
+                email: true,
+                profile: {
+                    select: {
+                        identity_type: true,
+                        identity_number: true,
+                        address: true
+                    }
+                }
+            }
+        })
+
+        if (users === null) {
+            let resp = ResponseTemplate(null, 'data not found', null, 404)
+            res.json(resp)
+            return
+        }
+
+        let resp = ResponseTemplate(users, 'success', null, 200)
+        res.json(resp)
+        return
+
+    } catch (error) {
+        let resp = ResponseTemplate(null, 'internal server error', error, 500)
+        res.json(resp)
+        return
+    }
+}
+
+async function Update(req, res) {
+
+    const { name, email, password, identity_type, identity_number, address } = req.body
+    const { id } = req.params
+
+    const payload = {}
+    const update = {}
+    const profile = {update}
+
+    if (!name && !email && !password && !identity_type && !identity_number && !address) {
+        let resp = ResponseTemplate(null, 'bad request', null, 400)
+        res.json(resp)
+        return
+    }
+
+    if (name) payload.name = name
+    if (email) payload.email = email
+    if (password) payload.password = password
+    if (identity_type || identity_number || address) payload.profile = profile
+    if (identity_type) update.identity_type = identity_type
+    if (identity_number) update.identity_number = identity_number
+    if (address) update.address = address
+
+    try {
+        const users = await prisma.user.update({
+            where: {
+                id: Number(id)
+            },
+            data: payload,
+            select: {
+                id: true,
+                name: true,
+                email: true,
+                profile: {
+                    select: {
+                        identity_type: true,
+                        identity_number: true,
+                        address: true
+                    }
+                }
+            }
+        })
+
+        let resp = ResponseTemplate(users, 'success', null, 200)
+        res.json(resp)
+        return
+
+    } catch (error) {
+        let resp = ResponseTemplate(null, 'internal server error', error, 500)
+        res.json(resp)
+        return
+    }
+}
+
+async function Delete(req, res) {
+
+    const { id } = req.params
+
+    try {
+
+        const checkUser = await prisma.user.findFirst({
+            where: {
+                id: Number(id)
+            }
+        })
+
+        if (checkUser === null) {
+            let resp = ResponseTemplate(null, 'data not found', null, 404)
+            res.json(resp)
+            return
+        }
+
+        await prisma.profile.delete({
+            where: {
+                user_id: Number(id)
+            },
+        })
+
+        await prisma.user.delete({
+            where: {
+                id: Number(id)
+            },
+        })
+
+        let resp = ResponseTemplate(null, 'data deleted', null, 200)
+        res.json(resp)
+        return
+
+    } catch (error) {
+        let resp = ResponseTemplate(null, 'internal server error', error, 500)
+        res.json(resp)
+        return
+    }
+}
+
+
+module.exports = {
+    Register,
+    Get,
+    GetByPK,
+    Update,
+    Delete
+}
--- src/routes/v2/user.route.js
+++ src/routes/v2/user.route.js
@@ -1,6 +1,173 @@
 const express = require('express')
 const router = express.Router()
+const { Register, Get, GetByPK, Update, Delete } = require('../../controller/user.controller')
+const { Auth } = require('../../middleware/middleware')
 
+const multer = require("multer")()
 
+/**
+ * @swagger
+ * /api/v2/users:
+ *   post:
+ *     security:
+ *      - bearerAuth: []
+ *     tags:
+ *      - "User"
+ *     summary: example to create user
+ *     requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                name:
+ *                  type: string
+ *                email:
+ *                  type: string
+ *                password:
+ *                  type: string
+ *                identity_type:
+ *                  type: string
+ *                identity_number:
+ *                  type: string
+ *                address:
+ *                  type: string
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *       404:
+ *         description: Not found
+ *       500:
+ *         description: Internal server error
+ */
+router.post('/users/', multer.single("image"), Register)
+
+/**
+ * @swagger
+ * /api/v2/users/:
+ *   get:
+ *     security:
+ *      - bearerAuth: []
+ *     tags:
+ *      - "User"
+ *     summary: Get all user
+ *     parameters:
+ *       - in: query
+ *         name: name
+ *         required: false
+ *         description: The name of the user
+ *         schema:
+ *           type: string
+ *       - in: query
+ *         name: email
+ *         required: false
+ *         description: The email of the user
+ *         schema:
+ *           type: string
+ *       - in: query
+ *         name: password
+ *         required: false
+ *         description: The password of the user
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *       404:
+ *         description: Not found
+ */
+router.get('/users/', Auth, Get)
+
+/**
+ * @swagger
+ * /api/v2/users/{id}:
+ *   get:
+ *     security:
+ *      - bearerAuth: []
+ *     tags:
+ *      - "User"
+ *     summary: Get one user
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The ID of the user
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *       404:
+ *         description: Not found
+ */
+router.get('/users/:id', Auth, GetByPK)
+
+/**
+ * @swagger
+ * /api/v2/users/{id}:
+ *   put:
+ *     security:
+ *      - bearerAuth: []
+ *     tags:
+ *      - "User"
+ *     summary: Get one user
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The ID of the user
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                name:
+ *                  type: string
+ *                email:
+ *                  type: string
+ *                password:
+ *                  type: string
+ *                identity_type:
+ *                  type: string
+ *                identity_number:
+ *                  type: string
+ *                address:
+ *                  type: string
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *       400:
+ *         description: Bad request
+ */
+router.put('/users/:id', Auth, Update)
+
+/**
+ * @swagger
+ * /api/v2/users/{id}:
+ *   delete:
+ *     security:
+ *      - bearerAuth: []
+ *     tags:
+ *      - "User"
+ *     summary: Get one user
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The ID of the user
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *       404:
+ *         description: Not found
+ */
+router.delete('/users/:id', Auth, Delete)
 
 module.exports = router
